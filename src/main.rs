428d0afc272dd559d201bd1815b7524f830b22c6
jermsam <ssali@jitpomi.com>
2022-08-24 22:49:15 -0600

    circumference or a circle of radius 0.43

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use rust_decimal_macros::dec;
+use rust_decimal::prelude::*;
 fn main() {
-    println!("Hello, world!");
+    let number = dec!(-1.23);
+    println!("Hello, world! {}",number);
+
+    let number_two = format!("{:.2}", 1.2399);
+    println!("Hello, world! {}", number_two);
+    println!("{:.2}", 1.2399);
+
+    let pi = Decimal::from_str("3.1415926535897932384626433832").unwrap();
+    let pi = pi.round_dp(2);
+    let k = Decimal::new(20,1);
+    let k = k.round();
+    let radius = Decimal::new(43,2);
+    let circumference = k * pi * radius;
+    println!("circumference = 2piR = {} x {} x {} = {}",k,pi,radius, circumference.round_dp(2))
 }
